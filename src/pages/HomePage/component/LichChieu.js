455eb4f1c1f1ce84db7ca22f09eff09c89b9adb8
kientrienhong <44995592+kientrienhong@users.noreply.github.com>
2020-08-31 20:16:28 +0700

    update phan cum rap lan 2

--- src/pages/HomePage/component/LichChieu.js
+++ src/pages/HomePage/component/LichChieu.js
@@ -3,13 +3,28 @@ import "./LichChieu.scss";
 export default function LichChieu(props) {
   const { film } = props;
 
-    const renderLichChieu = (listLichChieu) =>{
-        return listLichChieu.map((ele, i) =>{
-            return <div className="lichChieu__list-time__component" key={i}>
-                <div className="lichChieu__list-time__component__time">{ele.ngayChieuGioChieu}</div>
-            </div>
-        })
-    }
+  const filterLichChieu = (listLichChieu) => {
+    return listLichChieu.filter((ele) => {
+      return ele.ngayChieuGioChieu.includes("2019-01-02");
+    });
+  };
+
+  const convertToTime = (dateStr) =>{
+    const date = new Date(dateStr + "Z");
+    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })
+  }
+
+  const renderLichChieu = (listLichChieu) => {
+    return filterLichChieu(listLichChieu).map((ele, i) => {
+      return (
+        <div className="lichChieu__list-time__component" key={i}>
+          <div className="lichChieu__list-time__component__time">
+            {convertToTime(ele.ngayChieuGioChieu)}
+          </div>
+        </div>
+      );
+    });
+  };
 
   return (
     <div className="lichChieu">
@@ -18,10 +33,15 @@ export default function LichChieu(props) {
         src={film.hinhAnh}
         alt={film.tenPhim}
       ></img>
-      <div className="lichChieu__age">C13</div>
+      <div className="lichChieu__age">
+        <div className="lichChieu__age__info">C13</div>
+      </div>
       <div className="lichChieu__tenPhim">{film.tenPhim}</div>
+      <div className="lichChieu__loai-phim">
+        2D Digital
+      </div>
       <div className="lichChieu__list-time">
-          {renderLichChieu(film.lstLichChieuTheoPhim)}
+        {renderLichChieu(film.lstLichChieuTheoPhim)}
       </div>
     </div>
   );
